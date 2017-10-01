b59088d113fdfc9a0ba43d99797578773d0cac35
jprice628 <jprice628@hotmail.com>
2017-09-30 21:46:12 -0400

    Add player, ground, and obstacles to the scene.

---
+++ test-project/Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour
+{
+    public void Start()
+    {
+    }
+
+    public void Update()
+    {
+        var forward = Input.GetKey(KeyCode.W) ? 1.0f : 0.0f;
+        var backward = Input.GetKey(KeyCode.S) ? -1.0f : 0.0f;
+        var right = Input.GetKey(KeyCode.D) ? 1.0f : 0.0f;
+        var left = Input.GetKey(KeyCode.A) ? -1.0f : 0.0f;
+        var baseMovementVector = new Vector3(
+            right + left, 
+            0.0f, 
+            forward + backward);
+        var scaledMovementVector = baseMovementVector * Time.deltaTime;
+        transform.position = transform.position + scaledMovementVector;
+    }
+}
